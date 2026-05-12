a2ed1cc180047b64f0ec571ff7ad9fd44d2432bf
Mittapap <mbootphet@my.bcit.ca>
2026-05-12 09:07:26 -0700

    Add review schema

--- models/plant.js
+++ models/plant.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
-  username: String,
-  rating: Number,
-  fruitingStatus: String,
-  comment: String,
-  date: { type: Date, default: Date.now },
-});
-
 const plantSchema = new mongoose.Schema({
   fallingFruitId: { type: Number, required: true, unique: true },
   address: String,
-  unverified: Boolean,
   season_start: Number,
   season_stop: Number,
   lat: Number,
   lng: Number,
   photos: [String],
-  reviews: [reviewSchema],
 });
 
 const Plant = mongoose.model("Plant", plantSchema);
