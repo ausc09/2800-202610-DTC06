afe67527f0f38b3a212a81bf628083377fc97520
sebasabarca <sebasabarca_2012@outlook.com>
2026-05-14 17:57:22 -0700

    endpoint fix

--- routes/plantRoutes.js
+++ routes/plantRoutes.js
@@ -14,6 +14,14 @@ router.get("/plant/:id", async (req, res) => {
       return res.status(404).send("Plant not found");
     }
 
+    if (req.user) {
+      await viewHistory.create({
+        userId: req.user._id,
+        fallingFruitId: Number(id),
+        plantName: plant.name || "Unknown plant",
+      });
+    }
+
     res.render("plant", { plant });
   } catch (error) {
     console.log(error);
